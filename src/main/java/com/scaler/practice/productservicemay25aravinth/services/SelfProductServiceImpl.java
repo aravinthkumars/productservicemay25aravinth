a0900276ae870bc88d36f36d56233c22970c1fbd
Aravinth Kumar S <aravinthkumar.s@gmail.com>
2025-05-18 00:10:58 +0530

    DB Changes2

--- src/main/java/com/scaler/practice/productservicemay25aravinth/services/SelfProductServiceImpl.java
+++ src/main/java/com/scaler/practice/productservicemay25aravinth/services/SelfProductServiceImpl.java
@@ -1,6 +1,7 @@
 package com.scaler.practice.productservicemay25aravinth.services;
 
 import com.scaler.practice.productservicemay25aravinth.models.Product;
+import com.scaler.practice.productservicemay25aravinth.repositories.ProductRepository;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,13 @@ import java.util.List;
 @Service("selfProductService")
 //@Primary
 public class SelfProductServiceImpl implements ProductService {
+
+    private ProductRepository productRepository;
+
+    public SelfProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @Override
     public Product getSingleProduct(Long productId) {
         return null;
